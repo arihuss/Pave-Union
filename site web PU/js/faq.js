401163c545b5410127fe8698e65217f06e526bd9
sarahhannets <sarah.hann.1@ens.etsmtl.ca>
2025-06-18 17:41:39 -0400

    fix soumission

--- site web PU/js/faq.js
+++ site web PU/js/faq.js
@@ -1,20 +1,9 @@
-document.querySelectorAll('.faq-question').forEach(button => {
-  button.addEventListener('click', () => {
-    const answer = button.nextElementSibling;
-    const isActive = button.classList.contains('active');
 
- 
-    document.querySelectorAll('.faq-question').forEach(btn => {
-      btn.classList.remove('active');
+  document.querySelectorAll('.faq summary').forEach((q) => {
+    q.addEventListener('click', () => {
+      document.querySelectorAll('.faq details').forEach(d => {
+        if (d !== q.parentElement) d.removeAttribute('open');
+      });
     });
-    document.querySelectorAll('.faq-answer').forEach(ans => {
-      ans.classList.remove('show');
-    });
-
-   
-    if (!isActive) {
-      button.classList.add('active');
-      answer.classList.add('show');
-    }
   });
-});
+
