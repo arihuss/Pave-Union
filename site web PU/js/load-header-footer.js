f82b9b5adacbf75d1a0cf15712bd1572a04893b2
arihuss <ariane.hussaini.1@ens.etsmtl.ca>
2025-06-18 20:43:05 -0400

    add live chat

--- site web PU/js/load-header-footer.js
+++ site web PU/js/load-header-footer.js
@@ -58,3 +58,14 @@ window.addEventListener("scroll", function () {
     header.classList.remove("scrolled");
   }
 });
+
+
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/6853591fc96da61913b6f4fc/1iu2p899h';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
