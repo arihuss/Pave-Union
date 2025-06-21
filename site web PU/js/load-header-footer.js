29beccace2aefa36b6f7dc66cd868860c5f9cd8a
arihuss <ariane.hussaini.1@ens.etsmtl.ca>
2025-06-20 23:22:41 -0400

    enlever button rajouter fichier

--- site web PU/js/load-header-footer.js
+++ site web PU/js/load-header-footer.js
@@ -59,7 +59,6 @@ window.addEventListener("scroll", function () {
   }
 });
 
-
 var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 (function(){
 var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
